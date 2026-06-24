d2adf76ef34a87c2a9ba1950bb74d29963d1d085
perarasandeveloper <perarasang33@gmail.com>
2026-06-24 17:17:36 +0530

    Updated portfolio website

--- Portfolio/JS/index.js
+++ Portfolio/JS/index.js
@@ -5,14 +5,5 @@
         backSpeed: 50,
         loop: true
     });
-    let theme=document.getElementById("theme")
-    function themeChanger() {
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        theme.textContent = "☀️ Light Mode";
-    } else {
-        theme.textContent = "🌙 Dark Mode";
-    }
-}
+    
 
